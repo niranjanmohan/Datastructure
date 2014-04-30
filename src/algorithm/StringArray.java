9df939a765f0c388745e14f09b40ff05d68610d2
niranjanmohan <mohan.niranjan@gmail.com>
2014-04-29 23:16:45 -0400

    reorder

---
+++ src/algorithm/StringArray.java
@@ -0,0 +1,196 @@
+package algorithm;
+
+import java.util.Arrays;
+
+public class StringArray {
+	public static void main (String [] args){
+		//code to convert the number to base 10		
+		try{
+			System.out.println(Integer.parseInt("A",16));
+		}
+		catch(NumberFormatException nfe){
+			nfe.printStackTrace();
+		}
+
+		//code to check the String has got all unique digits
+
+		String un = "trusee";
+		for(int i=0;i<un.length();i++)
+		{
+			if(un.indexOf(un.charAt(i)) != (un.lastIndexOf(un.charAt(i)))){
+				System.out.println("duplicate");
+				break;
+			}	
+		}
+
+		//given 2 string write a function to see if one is the permutation of the other
+		String anagram1 ="created";
+		String anagram2 ="reactd";		
+		if(anagram1.length() == anagram2.length())
+			if(sorted(anagram1).equals(sorted(anagram2)))
+				System.out.println("Anagram");
+
+		if(checkAnagram(anagram1, anagram2))
+			System.out.println("char anagram");
+
+
+		//replace all spaces in a String with "%20"
+		System.out.println(replaceSpace("abc def "));
+
+		//using String for my own curiosity
+		String rep = "abc def ";
+		String newstr = rep.replaceAll(" ", "20%");
+		System.out.println(newstr);
+
+		//if string is in for aaaabbcc output should be a3b2c2 
+		compresString("aaabbcc");
+
+
+		//Rotate a matrix by 90 
+		int mat[][] = new int [4][4];
+		//create a matrix
+		for(int i=0;i<4;i++)
+			for(int j=0;j<4;j++)
+				mat[i][j]=j+1;
+
+		//print matrix
+		for(int i=0;i<4;i++){
+			System.out.println();
+			for(int j=0;j<4;j++){
+				System.out.print(mat[i][j]+" ");
+			}
+		}
+		
+		
+		rotateMatrix(mat);		
+
+
+	}
+
+	public static String sorted(String s){
+		char ar[] = s.toCharArray();
+		Arrays.sort(ar);
+		//System.out.println(String.valueOf(ar));
+		return String.valueOf(ar);
+	}
+	public static boolean checkAnagram(String s1,String s2){
+		//need to check using character array
+		boolean ar[]= new boolean[256];
+		for(int i=0;i<s1.length();i++){
+			int asci =s1.charAt(i);
+			ar[asci]= true;
+		}
+		for(int i=0;i<s2.length();i++){
+			int asci = s2.charAt(i);
+			if(! ar[asci])
+				return false;
+		}
+		return true;
+	}
+	public static String replaceSpace(String s){
+		char ar[]= s.toCharArray();
+		int count=0;
+		int oldlen = ar.length;
+		for(int i=0;i<ar.length;i++){
+			if (ar[i] ==' ')
+				count++;
+		}
+		System.out.println(count);
+		int newlen = count*2+ar.length;
+		//System.out.println("size before :"+ar.length);
+		ar =Arrays.copyOf(ar, newlen);
+		System.out.println("size"+ar.length);
+		for(int i=oldlen-1;i>=0;i--){
+			if(ar[i]==' '){
+				ar[newlen -1] = '%';
+				ar[newlen -2] = '0';
+				ar[newlen -3] = '2';
+				newlen-=3;
+			}
+			else{
+				ar[newlen-1] = ar[i];
+				newlen --;
+			}
+		}
+		return String.valueOf(ar);
+	}
+
+
+
+	//compress string aaabbcc -> a3b2c2
+	public static void compresString(String s){
+		StringBuffer bf = new StringBuffer(s);
+		StringBuffer f_bf = new StringBuffer();
+		char ch = bf.charAt(0);
+		f_bf.append(ch);
+		int count =1;
+		for(int i=1;i<bf.length();i++){
+			if(ch == bf.charAt(i)){
+				count ++;
+			}else{
+				f_bf.append(count);
+				ch = bf.charAt(i);
+				f_bf.append(ch);
+				count =1;
+			}	
+		}
+		f_bf.append(count);
+		if(f_bf.length() < bf.length())
+			System.out.println(f_bf.toString());
+		else
+			System.out.println(bf);
+	}
+	public static void rotateMatrix(int [][] mat){
+		System.out.println();
+		int size = mat.length;int top;
+		for(int layer =0;layer< size/2;++layer){
+			int first = layer;
+			int last = size-1-layer;
+			for(int i=first;i<size;++i){
+
+				System.out.println("first :"+first+" i :"+i+ "last :"+last);
+				//load the top to temp
+				top = mat[first][i];
+				int offset = i-first;
+				//load the left to top
+				mat[first][i] = mat[last-offset][first];
+				
+				//load the bottom to left 
+				mat[last-offset][first] = mat[last][last-offset];
+		
+				//load the right to bottom
+				mat[last][last-offset]=mat[i][last];
+			
+				//load the temp to right
+				mat[i][last] = top;
+				
+				/*
+				 *  layer :0 i :0last :3
+					layer :0 i :1last :3
+					layer :0 i :2last :3
+					layer :0 i :3last :3
+					layer :1 i :1last :3
+					layer :1 i :2last :3
+					layer :1 i :3last :3
+				 * 
+				 * 
+				 * 
+				 * 
+				 * 
+				 */
+				
+				
+				
+				
+			}
+		}
+		//print matrix
+		for(int i=0;i<4;i++){
+			System.out.println();
+			for(int j=0;j<4;j++){
+				System.out.print(mat[i][j]+" ");
+			}
+		}
+		
+	}
+}
---
+++ src/recursion/SimpleFunction.java
@@ -0,0 +1,146 @@
+package recursion;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class SimpleFunction {
+	public static void main(String args[]){
+
+		String s= "malllam";
+		test(5);
+		System.out.println("\n palindrome -->"+pal(s,0,s.length()-1));
+		pal(s);
+		fib(10);
+		System.out.println();
+		fibR(10);
+		fibDyn(10);
+		System.out.println(" \n decimal to binary");
+		dectobin(10);
+		dectobinIT(10);
+		System.out.println("in List :"+dectobin(101,new ArrayList<Integer>()));
+		System.out.println();
+		System.out.println(factorial(5));
+		factorialD(5);
+
+	}
+
+	//function prac1
+	public static void test(int n){
+		if(n>0){
+			System.out.print(n+" ");
+			test(n-1);
+			System.out.print(n+" ");
+		}
+		else return;
+	}
+	//palindrome recursive
+	public static boolean pal(String s,int i,int f){
+		if(i < f){
+			if(s.charAt(i) == s.charAt(f))
+				return pal(s,i+1,f-1);
+			else
+				return false;
+		}
+		else 
+			return true;
+	}
+	//palindrome iterative
+	public static void pal(String s){
+		int j = s.length()-1;
+		boolean flag = false;
+		for(int i=0;i<=s.length()/2-1;i++){
+			j = j-i;
+			if(s.charAt(i) == s.charAt(j))
+				flag = true;
+			else
+				flag = false;
+		}
+		System.out.println("fib test :"+flag);
+	}
+
+	//fibonacci
+	public static void fib(int n){
+		int f1=0,f2=0,fn=1;
+		f1=0;
+		System.out.println("fibonnaci for the number :");
+		while(f2 <= n){
+			System.out.print(f2+" ");
+			f1 = f2;
+			f2 = fn;
+			fn = f1+f2;
+		}
+	}
+	//fibonacci recursive
+	public static void fibR(int n,int f1,int f2){
+		if(n-f1 <= 0)
+			return ;
+		else{
+			 System.out.print(f1+" ");
+			 fibR(n,f2,f2+f1);
+		}
+	}
+	public static void fibR(int n){
+		fibR(n,0,1);
+	}
+	//fibonacci Dynamic
+	public static void fibDyn(int N){
+		long fib[] = new long[N+1];
+		//base case
+		fib[0] =0;
+		fib[1] =1;
+		for(int n=2;n<=N;n++){
+			fib[n] = fib[n-1] +fib[n-2];
+		}
+		System.out.println();
+		System.out.println("fibonacci--->"+fib[N]);
+	}
+	
+	
+	
+	
+	public static void dectobin(int n){
+		if(n >0){
+			dectobin(n/2);
+			System.out.print(n%2+" ");
+		}
+	}
+	public static void dectobinIT(int n){
+		int k =n;
+		System.out.println();
+		System.out.println("----------");
+		for(int i=31;i>=0;i--){
+			k = n>>i;
+		if((k&1) ==1)
+			System.out.print("1");
+		else
+			System.out.print("0");
+		}
+	}
+	public static List<Integer> dectobin(int n,List<Integer> nlist){
+		if(n >0){
+			dectobin(n/2,nlist);
+			nlist.add(n%2);
+		}
+		return nlist;
+	}
+	public static int factorialD(int n){
+		if(n == 0)
+			return 1;
+	else{
+			int result = factorialD(n-1);
+			int fact = n*result;
+			System.out.print(" \n factorial of "+n+"is :"+fact+ " intermediate value is :"+result);
+			return fact;
+		}
+	}
+	public static int factorial(int n){
+		if(n <=1)
+			return 1;
+		else
+			return n*factorial(n-1);
+	}
+
+
+
+}
